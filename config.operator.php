cb0e909598d70111627ee07d3c5029e157abc3ea
Marco <marchrist85@gmail.com>
2015-06-25 17:52:51 +0200

    aggiunti contratti

---
+++ config.operator.php
@@ -0,0 +1,11 @@
+<?php
+//map openwisp operator id from operator table with the odoo's user id in res_user table
+
+return array(
+	 '0' => '4'	 //luca pietroni
+	,'81' => '8' //italo pratelli
+	,'84' => '9' //lauretti
+	,'85' => '7' //vinicio
+	,'88'	=>	'10' //carmelo 
+
+);
